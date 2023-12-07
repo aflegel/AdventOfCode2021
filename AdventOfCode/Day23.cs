8ed60a17846d2b00b7d21a207bfacd37530e62b9
Alexander Boutin Flegel <alexander.boutinflegel@beca.com>
2023-12-08 08:31:04 +1300

    initial progress to save

---
+++ AdventOfCode/Day23.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode;
+
+public class Day23 : IAdventDay
+{
+	private class Hallway
+	{
+		public char[] RoomA { get; set; }
+		public char[] RoomB { get; set; }
+		public char[] RoomC { get; set; }
+		public char[] RoomD { get; set; }
+
+		public char[] HallwaySpaces { get; set; }
+	}
+	private Hallway Input { get; }
+
+	public Day23(string input)
+	{
+		var data = input.ReplaceLineEndings("\n").Split("\n").ToList();
+
+		Input = new Hallway
+		{
+			HallwaySpaces = Enumerable.Repeat(' ', 11).ToArray(),
+			RoomA = new[] { data[2][3], data[3][3] },
+			RoomB = new[] { data[2][5], data[3][5] },
+			RoomC = new[] { data[2][7], data[3][7] },
+			RoomD = new[] { data[2][9], data[3][9] }
+		};
+	}
+
+	public string Part1()
+	{
+		throw new NotImplementedException();
+	}
+
+	public string Part2()
+	{
+		throw new NotImplementedException();
+	}
+}
+
---
+++ AdventOfCodeTests/Day23Tests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using AdventOfCode;
+using FluentAssertions;
+
+namespace AdventOfCodeTests;
+
+public class Day23Tests
+{
+	private readonly string input = @"#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########";
+
+	[Fact]
+	public void Part1SouldMatchExampleCountB()
+	{
+		var day = new Day23(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("12521");
+	}
+
+	[Fact]
+	public void Part2SouldMatchExampleCountH()
+	{
+		var day = new Day23(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("2758514936282235");
+	}
+}
